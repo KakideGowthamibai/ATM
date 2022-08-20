ee25ad53038189b2e82af478bbe0625e170198a2
KakideGowthamibai <83656966+KakideGowthamibai@users.noreply.github.com>
2022-08-20 13:12:30 +0530

    Add files via upload

---
+++ src/ATMproject/AtmOperationInterface.java
@@ -0,0 +1,11 @@
+package ATMproject;
+
+public interface AtmOperationInterface {
+
+
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+	
+}
---
+++ src/ATMproject/MainClass.java
@@ -0,0 +1,61 @@
+package ATMproject;
+
+
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        AtmOperationInterface op=new AtmOperationImpl();
+        int atmnumber=93812;
+        int atmpin=122;
+        Scanner in=new Scanner(System.in);
+        System.out.println("Welcome to ATM Machine !!!");
+        System.out.print("Enter Atm Number : ");
+        int atmNumber=in.nextInt();
+        System.out.print("Enter Pin: ");
+        int pin=in.nextInt();
+        if((atmnumber==atmNumber)&&(atmpin==pin)){
+           while(true){
+        	
+         System.out.println("1.View Available Balance\n2.Withdraw Amount\n3.Deposit Amount\n4.viewMiniStatement\n5.Exit");
+                System.out.println("Enter Choice : ");
+                int choice=in.nextInt();
+                if(choice==1){
+                    op.viewBalance();
+
+                }
+                else if(choice==2){
+                    System.out.println("Enter amount to withdraw ");
+                    double withdrawAmount=in.nextDouble();
+                    op.withdrawAmount(withdrawAmount);
+
+                }
+                else if(choice==3){
+                    System.out.println("Enter Amount to Deposit :");
+                    double depositAmount=in.nextDouble();
+                    op.depositAmount(depositAmount);
+
+
+                }
+                else if(choice==4){
+                   op.viewMiniStatement();
+
+                }
+                else if(choice==5){
+                    System.out.println("Collect your ATM Card\n Thank you for using ATM Machine!!");
+                    System.exit(0);
+                }
+                else
+                {
+                    System.out.println("Please enter correct choice");
+                }
+            }
+        }
+        else{
+            System.out.println("Incorrect Atm Number or pin");
+            System.exit(0);
+        }
+
+
+    }
+}
